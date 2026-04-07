44003b664b307b7fd73011bab5dac2a41994ef1a
natively.dev <api@natively.dev>
2026-04-07 14:15:02 +0000

    fix: Correct Expo client import to prevent crash

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,4 +1,5 @@
-import { createAuthClient, expoClient } from "@better-auth/expo";
+import { createAuthClient } from "better-auth/react";
+import { expoClient } from "@better-auth/expo/client";
 import * as SecureStore from "expo-secure-store";
 import Constants from "expo-constants";
 
